7340c1f17f9d47dcbd28eec699ed4e60a0956b7e
PriyankaKumari007 <priyankakumari6709@gmail.com>
2025-08-11 20:03:15 +0530

    Basic data types

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Data Types
+   -int
+   -string
+   -number
+   -object
+2.let and const
+3.Array[index,value]
+4.Object[key,value]
---
+++ index.js
@@ -0,0 +1,23 @@
+let firstName = "Priyanka";
+let lastName = "Mahato";
+let age = 25;
+let isStudent = true;
+
+console.log(firstName + " " + lastName);
+console.log("Hello my first name is" +" "+firstName + " and my last name is " + lastName);
+
+//Array
+
+let arr=[1,"priyanka",true,4.5];
+console.log("Array",arr[0]);
+
+
+//Object
+let person = {
+    firstName: "Priyanka",
+    lastName: "Mahato",
+    age: 25,
+    company:"Namaste Dev",
+};
+
+console.log("object",person.firstName);
